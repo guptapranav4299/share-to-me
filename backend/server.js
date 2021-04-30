b3a8a391d872aff4f4d260054f7d2a62c2a80155
Pranav Gupta <guptapranav4299@gmail.com>
2021-04-30 23:52:19 +0530

    hide env files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+
+require('dotenv').config();
 const express = require("express")
 const app = express()
+const PORT = process.env.port || 3000
 
 const connectDB = require('./config/db');
 connectDB();
 
+app.use(express.json());
 
-const PORT = process.env.port || 3000
+// setting ejs templating engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`);
 })
