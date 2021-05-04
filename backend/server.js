898644d4796f2e9f2aa3202ff8cb53461b293f73
Pranav Gupta <guptapranav4299@gmail.com>
2021-05-04 23:53:57 +0530

    fixing deploy issues by mentioning the port number in env file

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require("express")
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 const path = require("path")
 
 
@@ -31,5 +31,5 @@ app.use('/files/download',require('./routes/download'))
 
 
 app.listen(PORT, () =>{
-    console.log(`Listening on port ${PORT}`);
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
